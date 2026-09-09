1cdbdc738fa970a52e9710fd1fc5d0e0260d22a5
Ananya Chaba <a.9520595675@gmail.com>
2026-09-09 15:14:38 +0530

    Add files via upload

---
+++ pallindrome_stack.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, temp, digit;
+    int stack[100];
+    int top = -1;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    temp = n;
+
+    while (temp > 0) {
+        digit = temp % 10;
+        top++;
+        stack[top] = digit;
+        temp = temp / 10;
+    }
+
+    temp = n;
+
+    while (temp > 0) {
+        digit = temp % 10;
+
+        if (digit != stack[top]) {
+            cout << "Not a palindrome";
+            return 0;
+        }
+
+        top--;
+        temp = temp / 10;
+    }
+
+    cout << "Palindrome";
+
+    return 0;
+}
